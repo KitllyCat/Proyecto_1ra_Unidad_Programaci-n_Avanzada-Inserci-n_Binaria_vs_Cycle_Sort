4637f313a6d4977a7f98eb4cb8573424e490ff5c
AlguienXD888 <lchurac@unjbg.edu.pe>
2025-10-19 23:51:57 -0500

    CPP Para ordenar arreglos de manera descendente

---
+++ Reverse_Orden.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+#include<fstream>
+#include<vector>
+using namespace std;
+
+int main(){
+	
+	string nombreArchivo;
+    cout<<"Ingrese el nombre del archivo .csv: ";
+    cin>>nombreArchivo;
+
+    ifstream archivo(nombreArchivo);
+    if (!archivo.is_open()){
+        cout<<"Error al abrir el archivo. Ingrese bien el nombre"<<endl;
+        return 1;
+    }
+
+    int cont=0;
+    int temp;
+    while (archivo >> temp){
+    	cont++;
+	}
+    archivo.close();
+    int n=cont;
+	int a[100000]; //Importante, modificar esto de acuerdo a la cantidad de numeros de n
+
+    archivo.open(nombreArchivo);
+    for (int i = 0; i < cont; i++) {
+        archivo >> a[i];
+    }
+    archivo.close();
+	
+	int aux, izq=0, der=n-1;
+	int m, j;
+	for(int i=1;i<n;i++){
+		aux=a[i];
+		izq=0;
+		der=i-1;
+		while(izq<=der){
+			m=(izq+der)/2;
+			if(aux<a[m]){
+				der=m-1;
+			}else{
+				izq=m+1;
+			}
+		}
+		j=i-1;
+		while(j>=izq){
+			a[j+1]=a[j];
+			j=j-1;
+		}
+		a[izq]=aux;
+	}
+	
+	for(int i=0;i<(n/2);i++){
+		int aux=a[n-i-1];
+		a[n-i-1]=a[i];
+		a[i]=aux;
+	}
+    
+    ofstream archivo1("RO.csv"); //Editable, se guardaran con diferentes nombres
+    
+    for (int i=0,j=0;i<n;i++){
+        archivo1<<a[i];
+        if (i!=n-1){
+			archivo1<<"\n";
+		}
+    }
+    archivo.close();
+ 
+	
+	return 0;
+}
